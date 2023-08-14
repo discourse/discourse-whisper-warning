3498dc13f683ca7cf2037538f857b584d6a65690
Jordan Vidrine <jordan@jordanvidrine.com>
2023-08-14 15:54:55 -0500

    FEATURE: Scope down to private categories & group PMs

--- javascripts/discourse/connectors/composer-action-after/whisper-warning.js
+++ javascripts/discourse/connectors/composer-action-after/whisper-warning.js
@@ -5,16 +5,44 @@ export default class WhisperWarning extends Component {
   @service currentUser;
 
   get shouldRender() {
-    
-    const groupMember = this.currentUser.groups.filter((group) => {
-      return group.name === "accidentalloudmouths";
-    }).length > 0;
-    const canWhisper = this.currentUser.whisperer;
-    const isNotNewTopic = this.args.outletArgs.model.get("action") !== "createTopic";
-    const isNotNewPM = this.args.outletArgs.model.get("action") !== "privateMessage";
-    const isNotSharedDraft = this.args.outletArgs.model.get("action") !== "createSharedDraft";
+    // checks if the current user is replying in a group PM
+    const allowedGroups =
+      this.args.outletArgs.model.topic?.get("allowedGroups");
+    // to check if reply is in a PM
+    const isPM = this.args.outletArgs.model.topic?.get("archetype") === "private_message";
+    // checks to make sure user is in group that PM is added to
+    const isInGroup =
+      this.currentUser.groups.filter(group => {
+        for (let allowedGroup of allowedGroups) {
+          if (group.name === allowedGroup.name) {
+            return group;
+          }
+        }
+      }).length > 0;
 
-    return canWhisper && isNotNewTopic && isNotNewPM && isNotSharedDraft && groupMember;
+    const readRestricted =
+      this.args.outletArgs.model.category?.get("read_restricted");
+    const groupMember =
+      this.currentUser.groups.filter(group => {
+        return group.name === "accidentalloudmouths";
+      }).length > 0;
+    const canWhisper = this.currentUser.whisperer;
+    const isNotNewTopic =
+      this.args.outletArgs.model.get("action") !== "createTopic";
+    const isNotNewPM =
+      this.args.outletArgs.model.get("action") !== "privateMessage";
+    const isNotSharedDraft =
+      this.args.outletArgs.model.get("action") !== "createSharedDraft";
+
+    return (
+      canWhisper &&
+      isNotNewTopic &&
+      isNotNewPM &&
+      isNotSharedDraft &&
+      groupMember &&
+      readRestricted ||
+      (isPM && isInGroup)
+    );
   }
 
   get isWhispering() {
