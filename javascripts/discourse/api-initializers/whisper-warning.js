e2540c02471cfacd738ae7c676a2b952990f2369
Jarek Radosz <jarek@cvx.dev>
2025-10-08 23:24:54 +0200

    DEV: Update linting

--- javascripts/discourse/api-initializers/whisper-warning.js
+++ javascripts/discourse/api-initializers/whisper-warning.js
@@ -1,6 +1,6 @@
 import { apiInitializer } from "discourse/lib/api";
 import whisperWarning from "../components/whisper-warning";
 
-export default apiInitializer("1.8.0", (api) => {
+export default apiInitializer((api) => {
   api.renderInOutlet("composer-action-after", whisperWarning);
 });
